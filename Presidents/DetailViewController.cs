14ff7c7b4632ef0c6ee587b8a6e3d00cefff24ae
Brian Bird <profbird@Mac-mini.local>
2015-05-18 09:19:48 -0700

    Ch. 11, Presidents mostly done, all working
    Everything works, except I need to remove the Edit button and make the
    master view show first.

---
+++ Presidents/DetailViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UIKit;
+using Foundation;
+
+namespace Presidents
+{
+	public partial class DetailViewController : UIViewController
+	{
+		public object DetailItem { get; set; }
+
+		public DetailViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public void SetDetailItem (object newDetailItem)
+		{
+			if (DetailItem != newDetailItem) {
+				DetailItem = newDetailItem;
+				
+				// Update the view
+				ConfigureView ();
+			}
+		}
+
+		void ConfigureView ()
+		{
+			// Update the user interface for the detail item
+			if (IsViewLoaded && DetailItem != null) {
+				var presDict = DetailItem as Dictionary<string, string>;
+
+				// Display the president's name
+				Title = presDict ["name"]; 
+
+				// Display the Wkikpedia URL for the selected president
+				detailDescriptionLabel.Text = presDict["url"];
+
+				// Display the Wikipedia article
+				var url = new NSUrl (presDict["url"]);
+				var request = new NSUrlRequest (url);
+				detailWebView.LoadRequest (request);
+			}
+
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			ConfigureView ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
