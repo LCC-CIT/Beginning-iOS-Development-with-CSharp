9db6fd1eab589f9e333d6d8b95c1c0d3a86655dc
Brian Bird <L00516163@cit132mac31.icl.lanecc.edu>
2015-05-27 09:59:06 -0700

    Ch. 12, BridgeControl, Added Label names
    Added names to the labels on the main view which also created outlets.
    I also removed the unit test project and the stuff in the Packages
    folder.

--- Chapter 12/BridgeControl/FirstViewController.designer.cs
+++ Chapter 12/BridgeControl/FirstViewController.designer.cs
@@ -1,18 +1,93 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace BridgeControl
 {
 	[Register ("FirstViewController")]
 	partial class FirstViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel authorizationCodeLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel favoriteAlienLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel favoriteCaptainLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel favoriteGadgetLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel favoriteTeaLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel officerLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel rankLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel warpDriveLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel warpFactorLabel { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (authorizationCodeLabel != null) {
+				authorizationCodeLabel.Dispose ();
+				authorizationCodeLabel = null;
+			}
+			if (favoriteAlienLabel != null) {
+				favoriteAlienLabel.Dispose ();
+				favoriteAlienLabel = null;
+			}
+			if (favoriteCaptainLabel != null) {
+				favoriteCaptainLabel.Dispose ();
+				favoriteCaptainLabel = null;
+			}
+			if (favoriteGadgetLabel != null) {
+				favoriteGadgetLabel.Dispose ();
+				favoriteGadgetLabel = null;
+			}
+			if (favoriteTeaLabel != null) {
+				favoriteTeaLabel.Dispose ();
+				favoriteTeaLabel = null;
+			}
+			if (officerLabel != null) {
+				officerLabel.Dispose ();
+				officerLabel = null;
+			}
+			if (rankLabel != null) {
+				rankLabel.Dispose ();
+				rankLabel = null;
+			}
+			if (warpDriveLabel != null) {
+				warpDriveLabel.Dispose ();
+				warpDriveLabel = null;
+			}
+			if (warpFactorLabel != null) {
+				warpFactorLabel.Dispose ();
+				warpFactorLabel = null;
+			}
 		}
 	}
 }
-
