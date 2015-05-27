149b806bee62b9abba662cc00646d2a02b98af48
Brian Bird <profbird@Mac-mini.local>
2015-05-27 08:39:43 -0700

    Ch. 12, BridgeControl, Rank pref complete
    Finished adding preferences to Root.plist up through Rank. Started the
    Main view- added 18 labels.

---
+++ Chapter 12/BridgeControl/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BridgeControl
+{
+	public class Constants
+	{
+		public const string OFFICER_KEY = "officer";
+		public const string AUTHORIZATION_CODE_KEY = "officer";
+		public const string RANK_KEY = "officer";
+		public const string WARP_DRIVE_KEY = "officer";
+		public const string FAVORITE_TEA_KEY = "officer";
+		public const string FAVORITE_CAPTAIN_KEY = "officer";
+		public const string FAVORITE_GADGET_KEY = "officer";
+		public const string FAVORITE_ALIEN_KEY = "officer";
+	}
+}
+
