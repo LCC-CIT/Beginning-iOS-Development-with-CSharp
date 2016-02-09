de7e4f0d374d15aa7d5a51d8e42f26455b786d0b
Brian Bird <profbird@gmail.com>
2016-02-08 19:56:08 -0800

    SingleComponentPIcker now works!
    There was something wrong with the picker component (or it’s
    properties). I deleted it from the storyboard and put a new one on and
    now it works!

--- Chapter-7/Pickers/SingleComponentPickerViewController.cs
+++ Chapter-7/Pickers/SingleComponentPickerViewController.cs
@@ -1,40 +1,24 @@
-﻿
-using System;
-
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace Pickers
 {
-	public partial class SingleComponentPickerViewController : UIViewController, IUIPickerViewDataSource
+	partial class SingleComponentPickerViewController : UIViewController, IUIPickerViewDataSource
 	{
-
 		private static string[] characterNames	= {"Luke", "Leia", "Han", "Chewbecca", "Artoo", "Threepio", "Lando"};
 
 		public SingleComponentPickerViewController (IntPtr handle) : base (handle)
 		{
 		}
 
-		/*
-		public SingleComponentPickerViewController() :base("SingleComponentPickerViewController", null)
-		{
-		}
-		*/
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-
 		public override void ViewDidLoad ()
 		{
+			singlePicker.DataSource = this;
 			singlePicker.Delegate = new SinglePickerViewDelegate();
 			base.ViewDidLoad ();
 		}
-			
 
 		partial void SelectButton_TouchUpInside (UIButton sender)
 		{
@@ -46,12 +30,12 @@ namespace Pickers
 			var action = UIAlertAction.Create("You're welcome", UIAlertActionStyle.Default, null);
 			alert.AddAction(action);
 			PresentViewController(alert, true, null);
-		}
 
+		}
 
 		// Picker Data Source Methods - implementing IUIPickerViewDataSource
 
-		 public  nint GetComponentCount(UIPickerView pickerView)  {
+		public  nint GetComponentCount(UIPickerView pickerView)  {
 			return 1;
 		}
 
@@ -59,16 +43,9 @@ namespace Pickers
 			return characterNames.GetLength(0);
 		}
 
-		// Picker delegate methods - implementing IUIPickerViewDelegate
-		// https://developer.xamarin.com/api/type/MonoTouch.UIKit.IUIPickerViewDelegate/
-		// All the protocols in the original UIPickerViewDelegate are optional - so there isn't anything to implement
-		// Instead, we'll create an instance of UIPickerViewDelegate and assign it to the picker's Delegate property
+		// Picker Delegate class (nested class)
 		class SinglePickerViewDelegate : UIPickerViewDelegate
 		{
-			public SinglePickerViewDelegate() {
-				// Just so I can put a breakpoint here
-			}
-
 			public override string GetTitle (UIPickerView pickerView, nint row, nint component)
 			{
 				return  SingleComponentPickerViewController.characterNames[row];
